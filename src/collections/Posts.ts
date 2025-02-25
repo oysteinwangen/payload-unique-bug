983a9d5f3dda42b9f22089ce672f61a790c02b20
oysteinwangen <oysteinwangen95@gmail.com>
2025-02-25 11:42:14 +0100

    Initial

---
+++ src/collections/Posts.ts
@@ -0,0 +1,22 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: 'text',
+      unique: true,
+      index: true,
+      required: true,
+    },
+  ],
+}
